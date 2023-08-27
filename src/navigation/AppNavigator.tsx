9e244b1a65816ea2d55dfbecc670df195a811bdd
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 23:11:53 +0530

    First Commit

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,7 +1,8 @@
 
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
 import DataInputScreen from '../screens/DataInputScreen';
 import DataDisplayScreen from '../screens/DataDisplayScreen';
 
