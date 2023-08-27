ad30f52562c78ce392776e304e1d28f5a7825cb5
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 23:11:52 +0530

    First Commit

--- src/screens/DataInputScreen.tsx
+++ src/screens/DataInputScreen.tsx
@@ -1,94 +1,69 @@
 
-import React from 'react';
-import { View, StyleSheet, TextInput, Button, Alert } from 'react-native';
-import { User, DataInputRequest } from '../types/Types';
-import { dataInputApi } from '../apis/DataInputApi';
+import React, { useState } from 'react';
+import { View, TextInput, Button, Alert } from 'react-native';
+import { DataInputRequest } from '../types/Types';
+import { DataInputApi } from '../apis/DataInputApi';
 
 const DataInputScreen: React.FC = () => {
-  const [user, setUser] = React.useState<User>({
-    userId: '',
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    address: '',
-  });
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
 
-  const handleInputChange = (field: keyof User, value: string) => {
-    setUser((prevUser) => ({
-      ...prevUser,
-      [field]: value,
-    }));
-  };
-
-  const handleSubmit = async () => {
+  const handleDataInput = async () => {
     try {
-      const request: DataInputRequest = {
-        user: user,
+      const requestData: DataInputRequest = {
+        user: {
+          firstName,
+          lastName,
+          email,
+          phone,
+          address,
+        },
       };
 
-      await dataInputApi(request);
-      Alert.alert('Success', 'Data input successful');
+      // Make API call to submit data input
+      const response = await DataInputApi.dataInput(requestData);
+
+      // Display success message
+      Alert.alert('Success', 'Data input submitted successfully');
     } catch (error) {
-      Alert.alert('Error', 'Failed to perform data input');
+      // Display error message
+      Alert.alert('Error', 'Failed to submit data input');
     }
   };
 
   return (
-    <View style={styles.container}>
+    <View>
       <TextInput
-        style={styles.input}
-        placeholder="User ID"
-        value={user.userId}
-        onChangeText={(value) => handleInputChange('userId', value)}
-      />
-      <TextInput
-        style={styles.input}
         placeholder="First Name"
-        value={user.firstName}
-        onChangeText={(value) => handleInputChange('firstName', value)}
+        value={firstName}
+        onChangeText={setFirstName}
       />
       <TextInput
-        style={styles.input}
         placeholder="Last Name"
-        value={user.lastName}
-        onChangeText={(value) => handleInputChange('lastName', value)}
+        value={lastName}
+        onChangeText={setLastName}
       />
       <TextInput
-        style={styles.input}
         placeholder="Email"
-        value={user.email}
-        onChangeText={(value) => handleInputChange('email', value)}
+        value={email}
+        onChangeText={setEmail}
       />
       <TextInput
-        style={styles.input}
         placeholder="Phone"
-        value={user.phone}
-        onChangeText={(value) => handleInputChange('phone', value)}
+        value={phone}
+        onChangeText={setPhone}
       />
       <TextInput
-        style={styles.input}
         placeholder="Address"
-        value={user.address}
-        onChangeText={(value) => handleInputChange('address', value)}
+        value={address}
+        onChangeText={setAddress}
       />
-      <Button title="Submit" onPress={handleSubmit} />
+      <Button title="Submit" onPress={handleDataInput} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-  input: {
-    marginBottom: 16,
-    padding: 8,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 4,
-  },
-});
 
 export default DataInputScreen;
