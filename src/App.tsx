3d8878b316feb8bc4a7712d9e58561a06830a6e5
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 23:11:51 +0530

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import AppNavigator from './navigation/AppNavigator';
+
+const Stack = createStackNavigator();
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <AppNavigator />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="AppNavigator"
+          component={AppNavigator}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
