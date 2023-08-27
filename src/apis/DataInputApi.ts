a6a6ddfe11cb1c0e18005315775cd5c542983d39
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 23:09:21 +0530

    First Commit

--- src/apis/DataInputApi.ts
+++ src/apis/DataInputApi.ts
@@ -2,17 +2,13 @@
 import axios from 'axios';
 import { DataInputRequest, DataInputResponse } from '../types/Types';
 
-const BASE_URL = 'YOUR_API_BASE_URL';
+const BASE_URL = 'YOUR_API_BASE_URL'; // Replace with your actual API base URL
 
-const DataInputApi = {
-  postDataInput: async (request: DataInputRequest): Promise<DataInputResponse> => {
-    try {
-      const response = await axios.post(`${BASE_URL}/data-input`, request);
-      return response.data;
-    } catch (error) {
-      throw new Error('Failed to perform data input');
-    }
-  },
+export const dataInputApi = async (request: DataInputRequest): Promise<DataInputResponse> => {
+  try {
+    const response = await axios.post<DataInputResponse>(`${BASE_URL}/data-input`, request);
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to perform data input');
+  }
 };
-
-export default DataInputApi;
