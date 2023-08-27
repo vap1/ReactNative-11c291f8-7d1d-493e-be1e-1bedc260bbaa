3601261d515780d9337818998fdd7db93aea2bef
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 23:09:17 +0530

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import DataInputScreen from './screens/DataInputScreen';
-import DataDisplayScreen from './screens/DataDisplayScreen';
-
-const Stack = createStackNavigator();
+import AppNavigator from './navigation/AppNavigator';
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="DataInput" component={DataInputScreen} />
-        <Stack.Screen name="DataDisplay" component={DataDisplayScreen} />
-      </Stack.Navigator>
+      <AppNavigator />
     </NavigationContainer>
   );
 };
