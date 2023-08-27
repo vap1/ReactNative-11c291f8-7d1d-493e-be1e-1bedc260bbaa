84fb2961fbd3fd645434f5e1b531d3f02b38e7bb
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 23:11:53 +0530

    First Commit

--- src/apis/DataDisplayApi.ts
+++ src/apis/DataDisplayApi.ts
@@ -4,11 +4,21 @@ import { DataDisplayRequest, DataDisplayResponse } from '../types/Types';
 
 const BASE_URL = 'https://0fg9aos700.execute-api.ap-south-1.amazonaws.com/asdfasdf/sample-route-key'; // Replace with your actual API base URL
 
-export const dataDisplayApi = async (request: DataDisplayRequest): Promise<DataDisplayResponse> => {
+export const getDataDisplay = async (): Promise<DataDisplayResponse> => {
   try {
     const response = await axios.get<DataDisplayResponse>(`${BASE_URL}/data-display`);
     return response.data;
   } catch (error) {
-    throw new Error('Failed to retrieve user details');
+    throw new Error('Failed to fetch data display');
+  }
+};
+
+export const dataDisplayApi = async (request: DataDisplayRequest): Promise<DataDisplayResponse> => {
+  try {
+    // Perform any necessary data transformations or validations before making the API call
+    const response = await getDataDisplay();
+    return response;
+  } catch (error) {
+    throw new Error('Failed to process data display request');
   }
 };
