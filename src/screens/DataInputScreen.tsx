e099b96d49a27a7699def5108ca51d517c800275
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 23:09:19 +0530

    First Commit

--- src/screens/DataInputScreen.tsx
+++ src/screens/DataInputScreen.tsx
@@ -1,11 +1,11 @@
 
-import React, { useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, TextInput, Button, Alert } from 'react-native';
 import { User, DataInputRequest } from '../types/Types';
-import dataInputApi from '../apis/DataInputApi';
+import { dataInputApi } from '../apis/DataInputApi';
 
 const DataInputScreen: React.FC = () => {
-  const [user, setUser] = useState<User>({
+  const [user, setUser] = React.useState<User>({
     userId: '',
     firstName: '',
     lastName: '',
@@ -14,50 +14,60 @@ const DataInputScreen: React.FC = () => {
     address: '',
   });
 
-  const handleInputChange = (key: keyof User, value: string) => {
+  const handleInputChange = (field: keyof User, value: string) => {
     setUser((prevUser) => ({
       ...prevUser,
-      [key]: value,
+      [field]: value,
     }));
   };
 
   const handleSubmit = async () => {
-    const request: DataInputRequest = {
-      user: user,
-    };
-
     try {
-      await dataInputApi.postDataInput(request);
-      // Show success message or navigate to another screen
+      const request: DataInputRequest = {
+        user: user,
+      };
+
+      await dataInputApi(request);
+      Alert.alert('Success', 'Data input successful');
     } catch (error) {
-      console.error('Failed to perform data input:', error);
-      // Show error message to the user
+      Alert.alert('Error', 'Failed to perform data input');
     }
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
+        style={styles.input}
+        placeholder="User ID"
+        value={user.userId}
+        onChangeText={(value) => handleInputChange('userId', value)}
+      />
+      <TextInput
+        style={styles.input}
         placeholder="First Name"
         value={user.firstName}
         onChangeText={(value) => handleInputChange('firstName', value)}
       />
       <TextInput
+        style={styles.input}
         placeholder="Last Name"
         value={user.lastName}
         onChangeText={(value) => handleInputChange('lastName', value)}
       />
       <TextInput
+        style={styles.input}
         placeholder="Email"
         value={user.email}
         onChangeText={(value) => handleInputChange('email', value)}
       />
       <TextInput
+        style={styles.input}
         placeholder="Phone"
         value={user.phone}
         onChangeText={(value) => handleInputChange('phone', value)}
       />
       <TextInput
+        style={styles.input}
         placeholder="Address"
         value={user.address}
         onChangeText={(value) => handleInputChange('address', value)}
@@ -66,5 +76,19 @@ const DataInputScreen: React.FC = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  input: {
+    marginBottom: 16,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+  },
+});
 
 export default DataInputScreen;
