621bc20bc841a93ae671cbac8874ad42360c7a2e
vap1 <vaibhavvasant1705@gmail.com>
2023-08-28 00:01:40 +0530

    First Commit

--- src/screens/DataDisplayScreen.tsx
+++ src/screens/DataDisplayScreen.tsx
@@ -1,13 +1,11 @@
 
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import { User } from '../types/Types';
 import { getDataDisplay } from '../apis/DataDisplayApi';
-import { search } from '../apis/SearchApi';
 
 const DataDisplayScreen: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
-  const [searchKeyword, setSearchKeyword] = useState('');
 
   useEffect(() => {
     fetchData();
@@ -22,34 +20,17 @@ const DataDisplayScreen: React.FC = () => {
     }
   };
 
-  const handleSearch = async () => {
-    try {
-      const response = await search(searchKeyword);
-      setUsers(response.searchResults);
-    } catch (error) {
-      console.error('Error searching data:', error);
-    }
-  };
-
   const renderItem = ({ item }: { item: User }) => (
     <View>
       <Text>{item.firstName} {item.lastName}</Text>
       <Text>Email: {item.email}</Text>
-      <Text>Phone: {item.phoneNumber}</Text>
+      <Text>Phone: {item.phone}</Text>
       <Text>Address: {item.address}</Text>
     </View>
   );
 
   return (
     <View>
-      <TextInput
-        placeholder="Search keyword"
-        value={searchKeyword}
-        onChangeText={setSearchKeyword}
-      />
-      <TouchableOpacity onPress={handleSearch}>
-        <Text>Search</Text>
-      </TouchableOpacity>
       <FlatList
         data={users}
         renderItem={renderItem}
