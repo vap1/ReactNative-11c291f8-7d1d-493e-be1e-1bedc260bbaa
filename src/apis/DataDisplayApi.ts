0114668d581778de68c62c675faa190cea3f0b18
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 23:10:50 +0530

    First Commit

--- src/apis/DataDisplayApi.ts
+++ src/apis/DataDisplayApi.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { DataDisplayRequest, DataDisplayResponse } from '../types/Types';
 
-const BASE_URL = 'YOUR_API_BASE_URL'; // Replace with your actual API base URL
+const BASE_URL = 'https://0fg9aos700.execute-api.ap-south-1.amazonaws.com/asdfasdf/sample-route-key'; // Replace with your actual API base URL
 
 export const dataDisplayApi = async (request: DataDisplayRequest): Promise<DataDisplayResponse> => {
   try {
